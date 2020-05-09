f6e57cadf35afaf7c758375025e7874ef3cd426e
cnattras <saccharomyces.cerevisae@gmail.com>
2020-05-09 16:56:25 -0400

    Fixing fits

--- fitcompareavg.bash
+++ fitcompareavg.bash
@@ -4,12 +4,15 @@ nice -n +19 root -b -l -q fitcompareavg1v2.cpp >& fitcompareavg1v2.txt
 nice -n +19 root -b -l -q fitcompareavg2v2.cpp >& fitcompareavg2v2.txt
 #11.5
 nice -n +19 root -b -l -q fitcompareavg3v2.cpp >& fitcompareavg3v2.txt
+#19.6
 nice -n +19 root -b -l -q fitcompareavg4v2.cpp >& fitcompareavg4v2.txt
 nice -n +19 root -b -l -q fitcompareavg5v2.cpp >& fitcompareavg5v2.txt
 nice -n +19 root -b -l -q fitcompareavg6v2.cpp >& fitcompareavg6v2.txt
 nice -n +19 root -b -l -q fitcompareavg7v2.cpp >& fitcompareavg7v2.txt
 nice -n +19 root -b -l -q fitcompareavg8v2.cpp >& fitcompareavg8v2.txt
+#62.4
 nice -n +19 root -b -l -q fitcompareavg9v2.cpp >& fitcompareavg9v2.txt
+#Still 62.4
 nice -n +19 root -b -l -q fitcompareavg10v2.cpp >& fitcompareavg10v2.txt
 nice -n +19 root -b -l -q fitcompareavg11v2.cpp >& fitcompareavg11v2.txt
 nice -n +19 root -b -l -q fitcompareavg12v2.cpp >& fitcompareavg12v2.txt
